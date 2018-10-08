9811f4ab9b0f56b329d226e1f52c2ea18885b691
gmerino92 <merino.gonza@gmail.com>
2018-10-08 00:50:46 -0300

    Small fix.

--- src/containers/CourseStudents.js
+++ src/containers/CourseStudents.js
@@ -10,7 +10,7 @@ export default class CourseStudents extends Component {
       this.props.history.push("/login") 
     }
   }
-
+ 
   render() {
     const childProps = {
       token: this.props.token,
